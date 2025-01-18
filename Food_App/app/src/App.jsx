812bf8e09afa839ff4ccce68c0834cb3a66cafee
DeepankarMalakar <deepankarmalakar007@gmail.com>
2025-01-18 15:00:14 +0530

    updated files

--- Food_App/app/src/App.jsx
+++ Food_App/app/src/App.jsx
@@ -1,13 +1,12 @@
 // import BASE_URL from "./assets/Constants";
 import FoodBody from "./Components/FoodBody";
-import Navbar from "./Components/Navbar";
+// import Navbar from "./Components/Navbar";
 // import { useEffect, useState } from "react";
 
 const App = () => {
 
   return (
     <>
-      <Navbar />
       <FoodBody />
     </>
   );
--- Food_App/app/src/Components/FoodBody.jsx
+++ Food_App/app/src/Components/FoodBody.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import FoodCard from "./FoodCard";
 import BASE_URL from "../assets/Constants";
+import Navbar from "./Navbar";
 
 function FoodBody() {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+    const [data, setData] = useState(null);             // for data
+    const [loading, setLoading] = useState(true);      // for loading
+    const [error, setError] = useState(null);          // for error
+    const [searchQuery, setSearchQuery] = useState(""); // for search input state
 
     useEffect(() => {
         const fetchFoodData = async () => {
@@ -15,7 +17,7 @@ function FoodBody() {
                     throw new Error("Failed to fetch data");
                 }
                 const json = await response.json();
-                console.log(json);
+                // console.log(json);
                 setData(json);
             } catch (error) {
                 setError("Unable to fetch data");
@@ -26,17 +28,29 @@ function FoodBody() {
         fetchFoodData();
     }, []);
 
+    const handleSearchChange = (event) => {
+        setSearchQuery(event.target.value.toLowerCase()); // Update search input
+    };
+
+    const filteredData = data?.filter((food) =>
+        food.name.toLowerCase().includes(searchQuery) // Filter based on search input
+    );
+
     if (error) return <div className="text-red-500 text-center mt-4">{error}</div>;
     if (loading) return <div className="text-center text-lg font-semibold mt-4">Loading...</div>;
 
     return (
-        <div className="bg-cover bg-center bg-[url('../src/assets/bg.png')] h-screen flex justify-center">
-            <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-                {data?.map((food) => (
-                    <FoodCard key={food?.name} foodData={food} />
-                ))}
+
+        <>
+            <Navbar handleSearch={handleSearchChange} />
+            <div className="bg-cover bg-center bg-[url('../src/assets/bg.png')] h-screen flex justify-center font-inter">
+                <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+                    {filteredData?.map((food) => (
+                        <FoodCard key={food?.name} foodData={food} />
+                    ))}
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
